c4e50a22b5ee376751663381fb56136780a804b4
Wilian Ansanello <75302006+wiliansanello@users.noreply.github.com>
2022-11-04 21:49:44 -0300

    progresso até a aula 4

--- mobile/src/screens/Pools.tsx
+++ mobile/src/screens/Pools.tsx
@@ -1,9 +1,14 @@
 import { VStack, Icon } from "native-base";
 import { Octicons } from "@expo/vector-icons"
+import { useNavigation } from "@react-navigation/native";
+
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
 
 export function Pools(){
+
+    const { navigate } = useNavigation();
+
     return (
         <VStack flex={1} bgColor="gray.900">
             <Header title="Meus bolões" />
@@ -11,6 +16,7 @@ export function Pools(){
                 <Button 
                     title="Buscar bolão pelo código"
                     leftIcon={<Icon as={Octicons} name="search" color="black" size="md"/>}
+                    onPress= { ()=> navigate("find")}
                 />
                  
             </VStack>
